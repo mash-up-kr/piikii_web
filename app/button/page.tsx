70cee9348a723d733b996e05e72d264dc7c5a034
장현봉 <wkdgusqhd080@naver.com>
2024-06-23 23:21:36 +0900

    feature: button page 추가

---
+++ app/button/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ButtonComponentPage = () => {
+  return (
+    <>
+      <div>여기에 카드 컴포넌트 쌓아둬주세요.구현해주세요.</div>
+    </>
+  );
+};
+
+export default ButtonComponentPage;
