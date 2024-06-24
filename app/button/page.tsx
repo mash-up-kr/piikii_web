eb3eee680f7cfe38662d5bd6813ce8bd6c983d51
장현봉 <wkdgusqhd080@naver.com>
2024-06-24 23:40:45 +0900

    #2 icon sample update

--- app/button/page.tsx
+++ app/button/page.tsx
@@ -1,11 +1,15 @@
 import { Button } from "@/components/common/Button/Button";
+import Image from "next/image";
 import React from "react";
 
 const ButtonComponentPage = () => {
   return (
     <div className="mt-10 mx-10">
       <Button>버튼</Button>
-      <Button variant={"secondary"}>삭제하기</Button>
+      <Button variant={"secondary"}>
+        <Image src="/trash.svg" alt="trash" width={24} height={24} />
+        삭제하기
+      </Button>
       <Button variant={"natural"}>투표시작</Button>
     </div>
   );
