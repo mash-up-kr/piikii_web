ee7dcdf7435ffe7f627326eb445a8c6e00c8dcc8
장현봉 <wkdgusqhd080@naver.com>
2024-06-25 20:55:01 +0900

    #2 ButtonProps interface name update

--- components/common/Button/Button.tsx
+++ components/common/Button/Button.tsx
@@ -68,13 +68,13 @@ const buttonVariants = cva(
   }
 );
 
-export interface ButtonButtonProps
+export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonButtonProps>(
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
     return (
