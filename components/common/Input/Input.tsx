ee80fd0884c2e79430b31fa3a45d11c408fbc42c
장현봉 <wkdgusqhd080@naver.com>
2024-06-26 12:19:46 +0900

    #11 Input Component Update

--- components/common/Input/Input.tsx
+++ components/common/Input/Input.tsx
@@ -10,12 +10,15 @@ const inputClassName = cva(
             w-full
             max-w-[335px]
             rounded-[12px]
+            border-color-natural-200
             border
             border-input
             bg-transparent
+            bg-natural-100
             px-[20px]
             py-[16px]
-            text-sm
+            text-fontSize-medium-16
+            text-color-natural-500
             shadow-sm
             transition-colors
             file:border-0
@@ -24,25 +27,40 @@ const inputClassName = cva(
             file:font-medium
             placeholder:text-muted-foreground
             focus-visible:outline-none
-            focus-visible:ring-1
+            focus-visible:ring-0
             focus-visible:ring-ring
+            focus:bg-natural-200
             disabled:cursor-not-allowed
             disabled:opacity-50
+            caret-Primary-700
             `
 );
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  leftSlot?: React.ReactNode;
+  rightSlot?: React.ReactNode;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, leftSlot, rightSlot, ...props }, ref) => {
+    const paddingLeft = leftSlot ? "pl-[40px]" : "";
+    const paddingRight = rightSlot ? "pr-[64px]" : "";
     return (
-      <input
-        type={type}
-        className={cn(inputClassName(), className)}
-        ref={ref}
-        {...props}
-      />
+      <div className="relative flex items-center w-full max-w-[335px]">
+        {leftSlot && (
+          <div className="absolute left-4 flex items-center">{leftSlot}</div>
+        )}
+        <input
+          type={type}
+          className={cn(inputClassName(), paddingLeft, paddingRight, className)}
+          ref={ref}
+          {...props}
+        />
+        {rightSlot && (
+          <div className="absolute right-4 flex items-center">{rightSlot}</div>
+        )}
+      </div>
     );
   }
 );
