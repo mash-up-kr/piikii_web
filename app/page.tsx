9e26d076400d7fc62184633762c60c79acda800e
장현봉 <wkdgusqhd080@naver.com>
2024-06-29 17:12:29 +0900

    feature: 인덱스페이지 접근시 home redirect 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import Image from "next/image";
-import Home from "./home/_components/Home";
+import { redirect } from "next/navigation";
 
-export default function HomePage() {
-  return (
-    // <div className="flex justify-center items-center h-full flex-col gap-[16px]">
-    //   <p className="text-black-22">이것은 홈이다</p>
-    //   <p className="text-semibold-20">아직은 개발중!</p>
-    //   <p className="text-regular-16">뚝딱뚝딱</p>
-    // </div>
-    <Home />
-  );
+export default function Home() {
+  return redirect("/home");
 }
