ec314f11618a9e2d88ba434e8e74b7fe21ed601c
Yung Ju Rick Kim <yungjurick@gmail.com>
2024-06-30 06:24:54 +0900

    feat: add overlay

--- components/common/Navigation/NavigationBar.tsx
+++ components/common/Navigation/NavigationBar.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/lib/utils";
+
 interface Props {
   /** 네비게이션 바 왼쪽 슬롯 - Padding은 외부에서 주입 */
   leftSlot?: React.ReactNode;
@@ -6,11 +8,22 @@ interface Props {
   rightSlot?: React.ReactNode;
 
   title?: string;
+  className?: string;
 }
 
-export default function NavigationBar({ leftSlot, rightSlot, title }: Props) {
+export default function NavigationBar({
+  leftSlot,
+  rightSlot,
+  title,
+  className,
+}: Props) {
   return (
-    <nav className="fixed w-[400px] flex items-center py-[16px] justify-between h-[56px] bg-neutral-0">
+    <nav
+      className={cn(
+        "fixed w-[375px] flex items-center py-[16px] justify-between h-[56px] bg-neutral-0",
+        className
+      )}
+    >
       <div>{leftSlot}</div>
       {title && (
         <h1 className="flex-1 text-center text-neutral-700 text-semibold-15">
