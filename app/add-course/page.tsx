8ad610c3e4fb2b86b91d64d5463b5a9c1e31eb28
장현봉 <wkdgusqhd080@naver.com>
2024-08-03 22:32:03 +0900

    add generateMetadata

--- app/add-course/page.tsx
+++ app/add-course/page.tsx
@@ -1,17 +1,44 @@
 import React from "react";
-import AddCourse from "./_components/AddCourse";
+import { Metadata, ResolvingMetadata } from "next";
 import { redirect } from "next/navigation";
+import AddCourse from "./_components/AddCourse";
 
-interface IAddCoursePageProps {
-  searchParams: { roomUid: string };
+type Props = {
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const roomUid = searchParams.roomUid;
+
+  const room = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/v1/rooms/${roomUid}`
+  ).then((res) => res.json());
+
+  return {
+    title: room.data.name,
+    description: room.data.message,
+    twitter: {
+      card: "summary_large_image",
+      title: room.data.name,
+      description: room.data.message,
+      images: `${room.data.thumbnailLinks}`,
+    },
+    openGraph: {
+      type: "website",
+      title: room.data.name,
+      description: room.data.message,
+      images: `${room.data.thumbnailLinks}`,
+    },
+  };
 }
 
-const AddCoursePage = ({ searchParams }: IAddCoursePageProps) => {
+const AddCoursePage = ({ searchParams }: Props) => {
   const hasRoomUid = !!searchParams.roomUid;
 
-  if (hasRoomUid) {
-    return <AddCourse />;
-  }
+  if (hasRoomUid) return <AddCourse />;
 
   return redirect("/");
 };
