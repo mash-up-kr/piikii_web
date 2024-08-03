1917420a7b346f181a9ab80ef2afde3ae3cd0fd3
Yung Ju Rick Kim <yungjurick@gmail.com>
2024-08-03 14:55:38 +0900

    feat: change query key parameter

--- apis/place/PlaceApi.query.ts
+++ apis/place/PlaceApi.query.ts
@@ -5,14 +5,13 @@ import { UseQueryParams } from "@/types/tanstack-query/use-query-params";
 import { useQuery } from "@tanstack/react-query";
 
 export const PLACE_API_QUERY_KEY = {
-  GET_PLACES: (params: Parameter<typeof placeApi.getPlaces>) =>
-    ["places", params.roomUid].filter(isNotNull),
+  GET_PLACES: (roomUid: string) => ["places", roomUid].filter(isNotNull),
 };
 
 export const useGetPlacesQuery = (
   params: UseQueryParams<typeof placeApi.getPlaces>
 ) => {
-  const queryKey = PLACE_API_QUERY_KEY.GET_PLACES(params.variables);
+  const queryKey = PLACE_API_QUERY_KEY.GET_PLACES(params.variables.roomUid);
 
   return useQuery({
     queryKey,
--- apis/vote/VoteApi.query.ts
+++ apis/vote/VoteApi.query.ts
@@ -1,20 +1,18 @@
-import { Parameter } from "@/types/utility/parameter";
 import voteApi from "./VoteApi";
 import { isNotNull } from "@/types/utility/is-not-null";
 import { UseQueryParams } from "@/types/tanstack-query/use-query-params";
 import { useQuery } from "@tanstack/react-query";
 
 export const VOTE_API_QUERY_KEY = {
-  GET_VOTES: (params: Parameter<typeof voteApi.getVotes>) =>
-    ["votes", params.roomUid].filter(isNotNull),
-  GET_VOTE_STATUS: (params: Parameter<typeof voteApi.getVoteStatus>) =>
-    ["vote", "status", params.roomUid].filter(isNotNull),
+  GET_VOTES: (roomUid: string) => ["votes", roomUid].filter(isNotNull),
+  GET_VOTE_STATUS: (roomUid: string) =>
+    ["vote", "status", roomUid].filter(isNotNull),
 };
 
 export const useGetVotesQuery = (
   params: UseQueryParams<typeof voteApi.getVotes>
 ) => {
-  const queryKey = VOTE_API_QUERY_KEY.GET_VOTES(params.variables);
+  const queryKey = VOTE_API_QUERY_KEY.GET_VOTES(params.variables.roomUid);
 
   return useQuery({
     queryKey,
@@ -26,7 +24,7 @@ export const useGetVotesQuery = (
 export const useGetVoteStatusQuery = (
   params: UseQueryParams<typeof voteApi.getVoteStatus>
 ) => {
-  const queryKey = VOTE_API_QUERY_KEY.GET_VOTE_STATUS(params.variables);
+  const queryKey = VOTE_API_QUERY_KEY.GET_VOTE_STATUS(params.variables.roomUid);
 
   return useQuery({
     queryKey,
