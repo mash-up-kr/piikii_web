ec3cf4aa1b69581f6ac394657849410b7161ac15
yihyun-kim1 <hyunnyfarm@naver.com>
2024-08-03 15:18:26 +0900

    feat: Add Course api and required files

---
+++ apis/course/CourseApi.query.ts
@@ -0,0 +1,22 @@
+import { Parameter } from "@/types/utility/parameter";
+import { isNotNull } from "@/types/utility/is-not-null";
+import { UseQueryParams } from "@/types/tanstack-query/use-query-params";
+import { useQuery } from "@tanstack/react-query";
+import courseApi from "./CourseApi";
+
+export const COURSE_API_QUERY_KEY = {
+  GET_COURSE: (params?: Parameter<typeof courseApi.checkCourses>) =>
+    ["course", params].filter(isNotNull),
+};
+
+export const useGetCourseQuery = (
+  params: UseQueryParams<typeof courseApi.checkCourses>
+) => {
+  const queryKey = COURSE_API_QUERY_KEY.GET_COURSE(params?.variables);
+
+  return useQuery({
+    queryKey,
+    queryFn: () => courseApi.checkCourses(params?.variables),
+    ...params?.options,
+  });
+};
