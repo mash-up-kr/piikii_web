fe4aa0a363ca3143fc25f84196ed79ff556cf89a
장현봉 <wkdgusqhd080@naver.com>
2024-08-08 22:56:38 +0900

    add useShare hook

---
+++ hooks/useShare.ts
@@ -0,0 +1,39 @@
+export type ShareDataType = {
+  url: string;
+  text?: string;
+  title?: string;
+  files?: File[];
+};
+
+const useShare = () => {
+  const isShareSupported = () => navigator?.share ?? false;
+
+  const copyToClipboard = (text: string, callback?: () => void) => {
+    navigator.clipboard.writeText(text).then(() => {
+      callback && callback();
+    });
+  };
+
+  /**
+   * Only Mobile && https
+   * @param data
+   */
+  const onShare = async (data: ShareDataType) => {
+    if (isShareSupported()) {
+      await window.navigator?.share({
+        ...data,
+        title: `📮 함께 온 메세지 ‘${data.title}’에 초대합니다. 💌`,
+      });
+    } else {
+      copyToClipboard(data?.url, () =>
+        alert("링크를 클립보드에 복사했습니다 :)")
+      );
+    }
+  };
+
+  return {
+    onShare,
+  };
+};
+
+export default useShare;
